3ed04775577d16ac5f8f43cf9d78ae84f20f7ba5
oak.du <dutiesheng@vddall.org>
2019-09-24 11:01:35 +0800

    baidu 统计

---
+++ baidutongji.js
@@ -0,0 +1,9 @@
+<script>
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?5e55779da2e08f6d9108f5eece199a16";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
+</script>
